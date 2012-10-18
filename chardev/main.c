f29abaadef3612744671942f8f4e0be467bec113
govind <govindarajulu90@gmail.com>
2012-10-18 13:46:36 +0530

    registered chrdev

--- chardev/main.c
+++ chardev/main.c
@@ -1,14 +1,28 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+#define MAJORR 59
+#define COUNT 2
+dev_t chrdev;
 static __init int modinit(void){
+    int err;
+    chrdev=MKDEV(MAJORR,0);
     printk("inserting module\n");
-    return 0;
+    printk("registering chrdev\n");
+    err=register_chrdev_region(chrdev,COUNT,"mychrdev");
+    if(err!=0)
+        goto error;
+        return 0;
+error:
+    return -1;
 }
 
 
 static __exit void modexit(void){
     printk("removing module\n");
+    printk("unregistering chrdev\n");
+    unregister_chrdev_region(chrdev,COUNT);
 }
 
 module_init(modinit);
