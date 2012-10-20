bff0eaf8d2a9b68b5a9cf3446fd0870a2662c770
govind <govindarajulu90@gmail.com>
2012-10-21 04:59:01 +0530

    i

--- chardev/main.c
+++ chardev/main.c
@@ -3,27 +3,39 @@
 #include <linux/kdev_t.h>
 #include <linux/io.h>
 #include <linux/fs.h>
-
-#define MAJORR 59
+#define MAJORR 1
 #define COUNT 2
+#define chrdev_name "mychrdev"
 dev_t chrdev;
+struct file_operations fop= {
+    .owner = THIS_MODULE
+
+};
 static __init int modinit(void){
     int err;
     chrdev=MKDEV(MAJORR,0);
+    printk("------------------------------\n");
     printk("inserting module\n");
-    printk("registering chrdev\n");
-    err=register_chrdev_region(chrdev,COUNT,"mychrdev");
-    if(err!=0)
-        goto error;
-        return 0;
-error:
-    return -1;
+    printk("trying registering chrdev:%s-MAJOR=%d,COUNT=%d\n",chrdev_name,MAJOR(chrdev),COUNT);
+    err=register_chrdev_region(chrdev,COUNT,chrdev_name);
+    if(err<0){
+        printk("chr register failed with register_chrdev_region, trying alloc_chrdev\n");
+        goto try_alloc_chrdev_region;
+    }
+    printk("registration successfull\n");
+    return 0;
+try_alloc_chrdev_region:
+    err=alloc_chrdev_region(&chrdev,0,COUNT,chrdev_name);
+    if(err<0)
+        return err;
+    printk("registed chrdev:%s-MAJOR=%d,COUNT=%d\n",chrdev_name,MAJOR(chrdev),COUNT);
+    return 0;
 }
 
 
 static __exit void modexit(void){
     printk("removing module\n");
-    printk("unregistering chrdev\n");
+    printk("unregistering chrdev:%s-MAJOR=%d,COUNT=%d\n",chrdev_name,MAJOR(chrdev),COUNT);
     unregister_chrdev_region(chrdev,COUNT);
 }
 
