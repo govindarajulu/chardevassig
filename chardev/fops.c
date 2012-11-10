4f4a5ca50afdb2db72975ce637f3359421c7f459
govind <govindarajulu90@gmail.com>
2012-11-11 03:05:22 +0530

    adding fops.c

---
+++ chardev/fops.c
@@ -0,0 +1,10 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kdev_t.h>
+#include <linux/io.h>
+#include <linux/fs.h>
+#include "chrdev.h"
+
+int myopen(struct inode *myinode, struct file *myfile) {
+    return 0;
+}
