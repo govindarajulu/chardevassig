c4b1b892bf75fa6b585e9ab4818d3668036ea9db
govind <govindarajulu90@gmail.com>
2012-11-12 22:40:10 +0530

    adding write

--- chardev/fops.c
+++ chardev/fops.c
@@ -4,6 +4,7 @@
 #include <linux/io.h>
 #include <linux/fs.h>
 #include <linux/string.h>
+#include <linux/slab.h>
 #include <asm-generic/uaccess.h>
 #include "chrdev.h"
 
@@ -15,8 +16,17 @@ int fops_release(struct inode *myinode, struct file *myfile) {
     return 0;
 }
 ssize_t fops_read(struct file* filep, char __user *buf, size_t count, loff_t *f_pos){
-    //strcpy(buf,"linux");
     copy_to_user(buf,"linux",6);
     *f_pos=*f_pos+6;
     return 6;
 }
+
+ssize_t fops_write(struct file* filep, char __user *buf, size_t count, loff_t *f_pos){
+    char *buffer;
+    buffer=kmalloc(count,GFP_KERNEL);
+    copy_from_user(buffer,buf,count);
+    printk("read- %s\n",buffer);
+    *f_pos=*f_pos+count;
+    return count;
+}
+
